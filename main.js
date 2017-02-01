f1a6ede5948449d872546cef7fe08f91680f679e
Tyler Foss <tylerscottfoss@gmail.com>
2017-02-01 01:43:01 -0700

    First node upload

---
+++ main.js
@@ -0,0 +1,24 @@
+var http = require("http");
+var fs = require("fs");
+var EventEmitter = require("events").EventEmitter;
+var port = 9000;
+var logger = new EventEmitter();
+
+logger.on("error", function(message){
+  console.log("ERROR:" + message);
+});
+
+http.createServer(function(request, response){
+  fs.readFile('index.html', function(err, content){
+    if(err){
+        response.writeHead(404, {'Content-Type': 'text/html'});
+        logger.emit("error", err);
+        response.end("failed");
+    }else{
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.end(content);
+    }
+  })
+}).listen(port);
+
+console.log('server running on port ' + port);
